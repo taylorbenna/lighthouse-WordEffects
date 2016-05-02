52039b6295c438d8a50fd5268f1616403d47d2b2
taylorbenna <taylor@benna.ca>
2016-05-02 14:15:15 -0700

    Setup

---
+++ Word Effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Effects
+//
+//  Created by Taylor Benna on 2016-05-02.
+//  Copyright © 2016 Taylor Benna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
